a04a4ce990c4b39d8e0f0530808ee7fe95d02240
Samsong <ssydyc@gmail.com>
2017-08-31 15:13:21 -0700

    Update Closest Number in Sorted Array.java

--- Binary Tree - Divide Conquer/Closest Number in Sorted Array.java
+++ Binary Tree - Divide Conquer/Closest Number in Sorted Array.java
@@ -1,1 +1,27 @@
-
+public class Solution {
+    /**
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     */
+    public int closestNumber(int[] A, int target) {
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = A.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (Math.abs(A[left] - target) > Math.abs(A[right] - target)) {
+            return right;
+        } else {
+            return left;
+        }
+    }
+}
