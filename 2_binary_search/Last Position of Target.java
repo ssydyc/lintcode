eb0cfc13a2c4d17322443db45d7f6f7e706b11bf
Samsong <ssydyc@gmail.com>
2017-09-01 11:03:21 -0700

    Create Last Position of Target.java

---
+++ 2_binary_search/Last Position of Target.java
@@ -0,0 +1,29 @@
+public class Solution {
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return an integer
+     */
+    public int lastPosition(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (nums[right] == target) {
+            return right;
+        } else if (nums[left] == target) {
+            return left;
+        } else {
+            return -1;
+        }
+    }
+}
