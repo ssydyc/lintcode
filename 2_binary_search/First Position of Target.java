3852be87191c4413e4a9b66e5d6735e7ba66698d
Samsong <ssydyc@gmail.com>
2017-09-01 19:21:02 -0700

    Create First Position of Target.java

---
+++ 2_binary_search/First Position of Target.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        } 
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        } else if (nums[right] == target) {
+            return right;
+        } else {
+            return -1;
+        }
+    }
+
+}
