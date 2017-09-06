5d886e9ede1a23ac5bd087ea2b67f7660350bf4c
Samsong <ssydyc@gmail.com>
2017-09-06 12:26:49 -0700

    Create Flatten Binary Tree to Linked List.java

---
+++ 3_Binary Tree and Divide Conquer/Flatten Binary Tree to Linked List.java
@@ -0,0 +1,39 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    
+    /*
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: 
+     */
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
+        if (root.left == null) {
+            return;
+        } else {
+            TreeNode temp = root.right;
+            root.right = root.left;
+            root.left = null;
+            while (root.right != null) {
+                root = root.right;
+            }
+            root.right = temp;
+        }
+    }
+    
+}
